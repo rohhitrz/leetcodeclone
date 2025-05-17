287bcb0b0e4bb3196076653b71359d29714090da
Rohit <rohhit.rz@gmail.com>
2025-05-17 10:59:35 +0530

    building leetlab backend

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,46 @@
+import bcryptjs from "bcryptjs";
+import { db } from "../libs/db.js";
+import { UserRole } from "../generated/prisma";
+
+
+export const register=async(req, res )=>{
+    const {email,name,password}=req.body;
+    try {
+        const existingUser= await db.user.findUnique({
+            where: {
+                email
+            }
+        })
+
+        if(existingUser){
+            return res.status(400).json({
+                "error": "user already exisit"
+            })
+        }
+
+        const hashedPassword=await bcrypt.hash(password,10);
+
+        const newUser= await db.user.create({
+            data:{
+                email,
+                password:hashedPassword,
+                name,
+                role: UserRole.USER
+            }
+        })
+    
+    } catch (error) {
+        console.log(error);
+        
+    }
+    
+}
+export const login=async(req, res )=>{
+
+}
+export const logout=async(req, res )=>{
+
+}
+export const check=async(req, res )=>{
+
+}
---
+++ backend/src/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+ res.send("welcome to leetlab");
+})
+
+app.use("/api/v1/auth", authRoutes);
+
+
+
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`port is running on ${PORT}`);
+});
